5875763185a442709814e07905bda09684b7c3aa
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-01-28 17:56:15 -0300

    alterado

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const router = express.Router()
+
+router.get('/sing_in', (req, res) => {
+    //res.sendFile(path.join(__dirname + 'paginadecadastro.html')) for example
+})
+
+
+router.get('/', (req, res) => {
+    res.send("Hello world")
+    
+})
+
+module.exports = router
