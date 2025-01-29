e7ef0e449aad3ff44f33f8e1eaa91c0909ba90d4
Wesley Silveira <wslc@cin.ufpe.br>
2025-01-29 01:12:06 -0300

    chore: Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,5 @@
 const router = express.Router()
 
-router.get('/sing_in', (req, res) => {
-    //res.sendFile(path.join(__dirname + 'paginadecadastro.html')) for example
-})
-
-
 router.get('/', (req, res) => {
     res.send("Hello world")
     
