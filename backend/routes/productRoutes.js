22ae214c645fd21859f518045aad8ddc5c670f54
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-01-31 21:28:05 -0300

    feat(productRoute): Adicionando rotas post de create e delete

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,9 @@ const productController = require('../controllers/productController')
 const dateMiddleware = require('../middlewares/product_Middleware')
 
 // Rota POST para criar produtos
-router.post("/post_create_product", dateMiddleware.date_validity, productController.create_product)
+router.post("/Post_create_product", dateMiddleware.date_validity, productController.create_product)
 
 // Rota POST para deletar produtos
-router.post("/post_delete_product", productController.delete_product)
+router.post("/Post_delete_product", productController.delete_product)
 
 module.exports = router
