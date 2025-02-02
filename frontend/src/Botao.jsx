bbec1b42b4fd8f15eb1d559a8aba68826388ddb3
Wesley Silveira <wslc@cin.ufpe.br>
2025-02-02 00:03:36 -0300

    chore(botão): trocando css por tailwindcss

--- frontend/src/Botao.jsx
+++ frontend/src/Botao.jsx
@@ -3,7 +3,13 @@ import {motion} from "framer-motion";
 
 const Botao = ({ texto }) => {
   return (<div className="container">
-    <motion.button className="botao-vermelho" whileHover={{ scale: 1.2 }}>{texto}</motion.button>
+    <motion.button
+      className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-red-700"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{scale: 0.9}}
+      >
+      {texto}
+    </motion.button>
   </div>);
 }
 
