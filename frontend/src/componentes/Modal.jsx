10c5d2f923eb1a215ca7a8c377d6f7a260b8a458
angelina santos <asc8@cin.ufpe.br>
2025-02-05 01:03:28 -0300

    feature modal

---
+++ frontend/src/componentes/Modal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+export default function ModalCC({ titulo, conteudo, isOpen, onClose, children }) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className="bg-white rounded-lg p-5 shadow-lg max-w-md w-full z-50 backdrop-blur-sm"
+      overlayClassName="fixed inset-0 bg-black/30 b backdrop-blur-sm flex justify-center items-center z-40"
+    >
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-semibold">{titulo}</h2>
+        {/* Botão do xis */}
+        <h1 className="cursor-pointer font-semibold text-2xl" onClick={onClose}>x</h1>
+      </div>
+      {/* Linha horizontal */}
+      <hr className="mt-2" />
+      {/* Aqui fica o conteúdo do modal que é passado dentro da tag do modal. Exemplo no perfil.jsx */}
+      {children}
+      
+    </Modal>
+  );
+}
--- frontend/src/paginas/Perfil.jsx
+++ frontend/src/paginas/Perfil.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import AuditCard from '../componentes/AuditCard'
-
+import React, { useState } from "react";
+import AuditCard from "../componentes/AuditCard";
+import Botao from "../componentes/Botao";
+import ModalCC from "../componentes/ModalCC";
 function Perfil() {
+  const [openModal, setOpenModal] = useState(false);
+  const toggleModal = () => setOpenModal((prev) => !prev);
   return (
-    <AuditCard
-    titulo={"O produto 'Nome do produto' foi adicionado ao estoque"}
-    horario={"18:30"}
-    data={"18/02/2025"}
-    funcaoClique={()=>{}}
-    ></AuditCard>
-  )
+    <>
+      <Botao texto={"Abrir modal"} onClick={toggleModal}></Botao>
+
+      <ModalCC titulo={"Teste"} isOpen={openModal} onClose={toggleModal}>
+        <div className="text-center">
+          <h3 className="text-lg font-semibold text-gray-800">
+            🎉 testezinho do modal 🎉
+          </h3>
+          <p className="text-gray-600 mt-2">
+            é só incluir o conteúdo do modal dentro da tag, isso é possível com
+            a prop chamada "children"
+          </p>
+        </div>
+      </ModalCC>
+    </>
+  );
 }
 
-export default Perfil
+export default Perfil;
