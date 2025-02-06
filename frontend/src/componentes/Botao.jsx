204111cef39681fc11f3da62268e14becce10466
Wesley Silveira <wslc@cin.ufpe.br>
2025-02-06 12:48:12 -0300

    ♻️ refactor(botão): adicionada função onClick

--- frontend/src/componentes/Botao.jsx
+++ frontend/src/componentes/Botao.jsx
@@ -1,11 +1,12 @@
 import {motion} from "framer-motion";
 
-const Botao = ({ texto }) => {
+const Botao = ({ texto, onClick }) => {
   return (<div className="container">
     <motion.button
       className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-red-700"
       whileHover={{ scale: 1.05 }}
       whileTap={{scale: 0.95}}
+      onClick={onClick}
       >
       {texto}
     </motion.button>
