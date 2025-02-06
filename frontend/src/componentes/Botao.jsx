1ed0d92b6d7272aca920b6817b0e071a7611fa17
Wesley Silveira <wslc@cin.ufpe.br>
2025-02-06 12:45:00 -0300

    🐛 fix(componentes): botão importando css inexistente

--- frontend/src/componentes/Botao.jsx
+++ frontend/src/componentes/Botao.jsx
@@ -1,12 +1,11 @@
-import "./index.css";
 import {motion} from "framer-motion";
 
 const Botao = ({ texto }) => {
   return (<div className="container">
     <motion.button
       className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-red-700"
-      whileHover={{ scale: 1.1 }}
-      whileTap={{scale: 0.9}}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{scale: 0.95}}
       >
       {texto}
     </motion.button>
