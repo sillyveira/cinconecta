75528541a3758116d7747c8de0d19c382831c051
Wesley Silveira <wslc@cin.ufpe.br>
2025-02-06 13:40:33 -0300

    💄style(componentes): melhor visualização para o auditCard

--- frontend/src/componentes/AuditCard.jsx
+++ frontend/src/componentes/AuditCard.jsx
@@ -4,17 +4,17 @@ import {motion} from  'framer-motion'
 function AuditCard({titulo, horario, data, funcaoClique}) {
   return (
     <>
-    <div className='text-black flex items-center gap-6 bg-gray-200 w-fit px-4 py-2 rounded-lg ml-5'>
+    <div className='text-black items-center justify-between flex w-fit gap-6 bg-gray-200  px-4 py-2 rounded-lg'>
       {/* DIV DO HORÁRIO/DATA */}
       <div className='place-items-center'> 
         <h1>{horario}</h1>
         <h1>{data}</h1>
       </div>
 
-      <div className="border-l border-gray-400 h-8" />
-      
-      {/* DIV DO TITULO */}
-      <h1 className='px-6'>{titulo}</h1>
+      <div className="border-1 h-8 border-gray-400" />
+
+      {/*DIV DO TITULO */}
+      <h1 className='px-6 max-w-[368px] text-center'>{titulo}</h1>
 
       <motion.button
             whileHover={{ scale: 1.1 }}
