4237036ddaf4fff3852301fb99de28d65495e77b
ifelp <iranildofpersonal18@gmail.com>
2025-02-10 18:36:40 -0300

    fix(stock bar): fixing overlap search bar problem.

--- frontend/src/componentes/stockBar.jsx
+++ frontend/src/componentes/stockBar.jsx
@@ -1,7 +1,6 @@
 import { ListFilter, CirclePlus, Search } from "lucide-react";
 import {InputAdornment, TextField} from '@mui/material'
 
-//TODO: Resolver problemas com a sobreposição da label e ativar o click dos botões
 //AVISO IMPORTANTE: A barra de pesquisa deste componente foi feita a partir do textfield da biblioteca material/mui, para renderizar este
 //componente, instale antes esta biblioteca (Emotion Engine)
 
@@ -21,7 +20,7 @@ export default function StockBar(){
 
             <TextField 
             className="w-[648px]"
-            label="Pesquisar"
+            placeholder="Pesquisar"
             color="black"
             slotProps={{
                 input:{
@@ -30,25 +29,22 @@ export default function StockBar(){
                             <Search color="black"/>
                         </InputAdornment>
                     )
-                },
-                inputLabel:{
-                    sx:{
-                        left: '50%',
-                        transform:'translateX(-50%)',
-                        textAlign: 'center',
-                        paddingTop:'12px',
-                        color:'black',
-                    }
                 }
-            }
-        }
-        sx={{
-            '& .MuiOutlinedInput-root': {
-              paddingTop: '8px',
-              borderRadius:'37px',
-              height:'43px',
-            },
-          }}
+            }}
+            sx={{
+                '& .MuiOutlinedInput-root': {
+                paddingTop: '4px',
+                borderRadius:'37px',
+                height:'43px',
+                },
+                '& .MuiInputBase-input::placeholder': {
+                    color: 'black',
+                    opacity: 1,
+                    left: '50%',
+                    transform:'translateX(-50%)',
+                    textAlign: 'center',
+                }
+            }}
             />
 
             <button onClick={clicou} className="cursor-pointer flex flex-row justify-start w-[138px] h-[43px] rounded-[24px] border-2 border-black gap-2 p-4">
