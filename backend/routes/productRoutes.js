4f30564d3e1b9d094a526536f18b02ed3789539a
ifelp <iranildofpersonal18@gmail.com>
2025-02-12 16:57:34 -0300

    docs(backend): removing backend directory.

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Exemplo de rota
-router.get('/', (req, res) => {
-  res.send('Rota de produtos funcionando!');
-});
-
-module.exports = router;
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-router.get('/', (req, res) => {
-    res.send("Rota de usuários funcionando!")
-    
-})
-
-module.exports = router
