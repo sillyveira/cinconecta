0e9bccc85e2c94d2099d609f2f50ee6f79c9b7ed
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-02-13 20:37:11 -0300

    Fix(produto): restaurando o código de update_product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,7 @@ router.post("/criar-produto", authMiddleware, productController.create_product)
 // Rota PUT para atualizar produtos
 router.put("/atualizar-produto/:id", authMiddleware, productController.update_product)
 
-// Rota DELETE para excluir produtos
+// Rota POST para EXCLUIR produtos
 router.post("/deletar-produto", authMiddleware, productController.delete_product)
 
 module.exports = router
