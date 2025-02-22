03010465088e347268f55b48f02f557d312d2fee
Wesley Silveira <wslc@cin.ufpe.br>
2025-02-22 10:18:18 -0300

    ✨ feat(modal): Validações no modal de categoria

--- frontend/src/componentes/ModalCategoria.jsx
+++ frontend/src/componentes/ModalCategoria.jsx
@@ -10,6 +10,7 @@ import {
   deletarCategoria,
 } from "../servicos/DataAPI";
 import { useAuth } from "../contextos/AuthContext";
+import toast from "react-hot-toast";
 
 export default function ModalCategoria({ open, onRequestClose }) {
   const [modo, setModo] = useState("criar"); // 'criar', 'editar', 'remover'
@@ -43,6 +44,46 @@ export default function ModalCategoria({ open, onRequestClose }) {
     setCategorias(mapearParaLista(Categorias));
   }, [Categorias]);
 
+  const [erros, setErros] = useState({
+    titulo: "",
+    categoria: "",
+  });
+
+  const isFormValid = () => {
+    return erros.titulo == "" && erros.categoria == "";
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // Validação da criação de categoria
+
+    if (name == "criar-categoria" && value.length <= 25) {
+      setInfoCategoria((prev) => [
+        { ...prev[0], criarCategoria: value }, // Atualiza apenas criarCategoria
+        prev[1], // Mantém o segundo objeto inalterado
+        prev[2], // Mantém o terceiro objeto inalterado
+      ]);
+      setErros((prev) => ({ ...prev, titulo: "" }));
+    } else if (name == "criar-categoria") {
+      setErros((prev) => ({
+        ...prev,
+        titulo: "A categoria excede 25 caracteres.",
+      }));
+    }
+
+    if (name == "editar-categoria" && value.length <= 25) {
+      setInfoCategoria((prev) => [
+        prev[0], // Mantém o primeiro objeto inalterado
+        { ...prev[1], editarCategoria: value }, // Atualiza apenas editarCategoria
+        prev[2], // Mantém o terceiro objeto inalterado
+      ]);
+      setErros((prev) => ({ ...prev, titulo: "" }));
+    } else if (name == "editar-categoria") {
+      setErros((prev) => ({
+        ...prev,
+        titulo: "A categoria excede 25 caracteres.",
+      }));
+    }
+  };
   let conteudo = null;
   if (modo === "criar") {
     conteudo = (
@@ -50,15 +91,10 @@ export default function ModalCategoria({ open, onRequestClose }) {
         <div className="flex items-center justify-center">
           <input
             type={"text"}
+            name="criar-categoria"
             className="pl-4 w-60 h-10 border-2xl border-[#B6B6B6] bg-[#B6B6B6] text-white rounded-xl placeholder-white focus:outline-none"
             placeholder={"Título da nova categoria"}
-            onChange={(event) => {
-              setInfoCategoria((prev) => [
-                { ...prev[0], criarCategoria: event.target.value }, // Atualiza apenas criarCategoria
-                prev[1], // Mantém o segundo objeto inalterado
-                prev[2], // Mantém o terceiro objeto inalterado
-              ]);
-            }}
+            onChange={handleChange}
             value={infoCategoria[0].criarCategoria}
           />
 
@@ -66,15 +102,26 @@ export default function ModalCategoria({ open, onRequestClose }) {
           <Botao
             className={"mx-2 my-2"}
             texto={"Adicionar"}
+            disabled={!isFormValid()}
             onClick={async () => {
+
+              if (infoCategoria[0].criarCategoria == "") {
+                toast.error("Selecione um novo título para sua categoria!");
+                return;
+              }
+
               await adicionarCategoria(
                 logout,
                 infoCategoria[0]?.criarCategoria,
                 carregarCategorias
               );
+              onRequestClose();
             }}
           ></Botao>
         </div>
+        <p className="text-red-500 text-sm min-h-[20px] text-center">
+          {erros.titulo}
+        </p>
       </>
     );
   } else if (modo === "editar") {
@@ -98,25 +145,39 @@ export default function ModalCategoria({ open, onRequestClose }) {
         <div className="flex items-center justify-center">
           <input
             type={"text"}
+            name="editar-categoria"
             className="pl-4 w-60 h-10 border-2xl border-[#B6B6B6] bg-[#B6B6B6] text-white rounded-xl placeholder-white focus:outline-none"
             placeholder={"Título da nova categoria"}
-            onChange={(event) => {
-              setInfoCategoria((prev) => [
-                prev[0], // Mantém o primeiro objeto inalterado
-                { ...prev[1], editarCategoria: event.target.value }, // Atualiza apenas editarCategoria
-                prev[2], // Mantém o terceiro objeto inalterado
-              ]);
-            }}
+            onChange={handleChange}
             value={infoCategoria[1].editarCategoria}
           />
           <Botao
             className={"mx-2 my-2"}
             texto={"Editar"}
-            onClick={async() => {
-                await atualizarCategoria(logout, infoCategoria[1]?._idCategoriaEditar, infoCategoria[1]?.editarCategoria, carregarCategorias)
+            disabled={!isFormValid()}
+            onClick={async () => {
+              if (infoCategoria[1]._idCategoriaEditar == "") {
+                toast.error("Selecione uma categoria para ser editada!");
+                return;
+              }
+
+              if (infoCategoria[1].editarCategoria == "") {
+                toast.error("Selecione um novo título para sua categoria!");
+                return;
+              }
+              await atualizarCategoria(
+                logout,
+                infoCategoria[1]?._idCategoriaEditar,
+                infoCategoria[1]?.editarCategoria,
+                carregarCategorias
+              );
+              onRequestClose();
             }}
           ></Botao>
         </div>
+        <p className="text-red-500 text-sm min-h-[20px] text-center">
+          {erros.titulo}
+        </p>
       </>
     );
   } else if (modo === "remover") {
@@ -129,7 +190,7 @@ export default function ModalCategoria({ open, onRequestClose }) {
               setInfoCategoria((prev) => [
                 prev[0], // Mantém o primeiro objeto inalterado
                 prev[1], // Atualiza apenas editarCategoria
-                {...prev[2], _idCategoriaRemover: v}, // Mantém o terceiro objeto inalterado
+                { ...prev[2], _idCategoriaRemover: v }, // Mantém o terceiro objeto inalterado
               ])
             }
             className="w-88"
@@ -141,8 +202,18 @@ export default function ModalCategoria({ open, onRequestClose }) {
           <Botao
             className={"mx-2 my-2 w-34"}
             texto={"Remover"}
-            onClick={async() => {
-                await deletarCategoria(logout, infoCategoria[2]?._idCategoriaRemover, carregarCategorias)
+            onClick={async () => {
+              console.log(infoCategoria[2]._idCategoriaRemover);
+              if (infoCategoria[2]._idCategoriaRemover == "") {
+                toast.error("Selecione uma categoria para ser removida!");
+                return;
+              }
+
+              await deletarCategoria(
+                logout,
+                infoCategoria[2]?._idCategoriaRemover,
+                carregarCategorias
+              );
             }}
           ></Botao>
         </div>
@@ -155,15 +226,16 @@ export default function ModalCategoria({ open, onRequestClose }) {
       isOpen={open}
       onClose={onRequestClose}
     >
-      <div className="flex gap-3 mb-4 justify-center">
+      <div className="flex gap-3 mb-4 mt-4 justify-center">
         <Botao
           texto="Criar"
           onClick={() => {
             setInfoCategoria((prev) => [
-              { ...prev[0], criarCategoria: "" }, // Atualiza apenas criarCategoria
-              prev[1], // Mantém o segundo objeto inalterado
-              prev[2], // Mantém o terceiro objeto inalterado
+              { ...prev[0], criarCategoria: "" }, 
+              prev[1],
+              prev[2],
             ]);
+            setErros({titulo: "", categoria: ""})
             setModo("criar");
           }}
         />
@@ -175,6 +247,7 @@ export default function ModalCategoria({ open, onRequestClose }) {
               { ...prev[1], editarCategoria: "", _idCategoriaEditar: "" }, // Atualiza apenas editarCategoria
               prev[2], // Mantém o terceiro objeto inalterado
             ]);
+            setErros({titulo: "", categoria: ""})
             setModo("editar");
           }}
         />
@@ -186,6 +259,7 @@ export default function ModalCategoria({ open, onRequestClose }) {
               prev[1], // Atualiza apenas editarCategoria
               { ...prev[2], _idCategoriaRemover: "" }, // Mantém o terceiro objeto inalterado
             ]);
+            setErros({titulo: "", categoria: ""})
             setModo("remover");
           }}
         />
