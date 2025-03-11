7afa867d78438162102bf35e78a474fd6b55020b
Wesley Silveira <wslc@cin.ufpe.br>
2025-03-11 14:39:24 -0300

    Em desenvolvimento 🚧

--- frontend/src/componentes/EntradaSaida.jsx
+++ frontend/src/componentes/EntradaSaida.jsx
@@ -25,7 +25,7 @@ const ultimosMeses = [
   { name: "Dez", entrada: 30, saida: 9 },
 ];
 export default function EntradaSaida() {
-  const {Dados} = useContext(DataContext);
+  const { Dados } = useContext(DataContext);
   return (
     <motion.div
       className="text-black bg-gray-800 shadow-2xl bg-opacity-100 border backdrop-blur-md rounded-md col-span-3"
@@ -33,45 +33,53 @@ export default function EntradaSaida() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.1 }}
     >
-      <h2 className="text-white text-lg font-medium text-center">
-        Gráfico de entrada e saída
-      </h2>
+      <div className="flex items-center justify-center gap-2 py-1">
+        <h2 className="text-white text-lg font-medium text-center">
+          Gráfico de entrada e saída
+        </h2>
+        <select className="bg-white rounded-2xl pl-1 text-center">
+          <option value="">3 meses</option>
+          <option value="">6 meses</option>
+          <option value="">1 ano</option>
+        </select>
+      </div>
+
       <div className="h-48">
-      {(!Dados || Dados.length === 0) ? ( // Verifica se Grafico é null/undefined ou vazio
-        <p>Sem gráfico disponível</p>
-      ) : (
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart data={Dados.grafico} margin={{ right: 40 }}>
-            <CartesianGrid strokeDasharray="3 3" stroke="#FFFFFFFF" />
-            <XAxis color="#FFFFFFF" stroke="#EDFFECFF" dataKey="name" />
-            <YAxis domain={[0, 45]} stroke="#EDFFECFF" dataKey="entrada" />
-            <Tooltip
-              contentStyle={{
-                backgroundColor: "rgba(31,41,55,0.8)",
-                borderColor: "#4B5563",
-              }}
-              labelStyle={{ color: "#FFFFFF" }}
-              itemStyle={{ color: "#FFFFFF" }}
-            />
-            <Line
-              type="monotone"
-              dataKey="entrada"
-              stroke="#76F163FF"
-              strokeWidth={3}
-              dot={{ fill: "#76F163FF", strokeWidth: 2, r: 6 }}
-              activeDot={{ r: 8, strokeWidth: 2 }}
-            />
-            <Line
-              type="monotone"
-              dataKey="saida"
-              stroke="#F16363FF"
-              strokeWidth={3}
-              dot={{ fill: "#F16363FF", strokeWidth: 1, r: 6 }}
-              activeDot={{ r: 8, strokeWidth: 2 }}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      )}
+        {!Dados || Dados.length === 0 ? ( // Verifica se Grafico é null/undefined ou vazio
+          <p>Sem gráfico disponível</p>
+        ) : (
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={Dados.grafico} margin={{ right: 40 }}>
+              <CartesianGrid strokeDasharray="3 3" stroke="#FFFFFFFF" />
+              <XAxis color="#FFFFFFF" stroke="#EDFFECFF" dataKey="name" />
+              <YAxis domain={[0, 45]} stroke="#EDFFECFF" dataKey="entrada" />
+              <Tooltip
+                contentStyle={{
+                  backgroundColor: "rgba(31,41,55,0.8)",
+                  borderColor: "#4B5563",
+                }}
+                labelStyle={{ color: "#FFFFFF" }}
+                itemStyle={{ color: "#FFFFFF" }}
+              />
+              <Line
+                type="monotone"
+                dataKey="entrada"
+                stroke="#76F163FF"
+                strokeWidth={3}
+                dot={{ fill: "#76F163FF", strokeWidth: 2, r: 6 }}
+                activeDot={{ r: 8, strokeWidth: 2 }}
+              />
+              <Line
+                type="monotone"
+                dataKey="saida"
+                stroke="#F16363FF"
+                strokeWidth={3}
+                dot={{ fill: "#F16363FF", strokeWidth: 1, r: 6 }}
+                activeDot={{ r: 8, strokeWidth: 2 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </div>
     </motion.div>
   );
