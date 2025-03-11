86247e54bafc36d82f866d991cfdac7b8718848d
Wesley Silveira <wslc@cin.ufpe.br>
2025-03-11 14:18:12 -0300

    ♻ refactor(auth): Função de login refatorada

--- backend/tests/controllers/userController.test.js
+++ backend/tests/controllers/userController.test.js
@@ -8,9 +8,11 @@ const axios = require("axios");
 const MockAdapter = require("axios-mock-adapter");
 const Audit = require("../../models/Audit");
 const Session = require("../../models/Session");
+const { revogarSessao } = require("../../services/sessionService");
+const { logout, members } = require("../../controllers/userController");
 require("dotenv").config({ path: "../../.env" });
 
-describe("Auth Controller (Autenticação)", () => {
+describe("UserController (Autenticação)", () => {
   let mock;
   beforeAll(async () => {
     mock = new MockAdapter(axios);
@@ -195,8 +197,6 @@ describe("Auth Controller (Autenticação)", () => {
     expect(session).not.toBeNull();
     expect(session.token).toBeDefined();
     expect(session.expira_em).toBeInstanceOf(Date);
-
-    
   });
 
   test("Login - deve autenticar um usuário existente com sessão criada", async () => {
@@ -206,17 +206,17 @@ describe("Auth Controller (Autenticação)", () => {
       user: { name: "Usuário Teste" },
       ngo: { id: "123", name: "ONG Existente" },
     });
-  
+
     // Criando uma ONG no banco
     const ong = await Ong.create({ id_gov: 123, nome: "ONG Existente" });
-  
+
     // Criando um usuário no banco
     const user = await User.create({
       email: "teste@email.com",
       nome: "Usuário Teste",
       id_ong: ong._id,
     });
-  
+
     // Criando uma sessão para o usuário
     const session = await Session.create({
       id_usuario: user._id,
@@ -224,28 +224,101 @@ describe("Auth Controller (Autenticação)", () => {
       nome_usuario: user.nome,
       token: "token_valido_123",
     });
-  
+
     // Garantir que a sessão foi criada
     const sessionExists = await Session.findOne({ id_usuario: user._id });
     expect(sessionExists).not.toBeNull();
     expect(sessionExists.token).toBe("token_valido_123");
-  
+
     // Execução da rota de login
     const res = await request(app)
       .post("/usuarios/login")
       .send({ email: "teste@email.com", password: "senha123" });
-  
+
     // Verificar se a resposta é bem-sucedida (200)
     expect(res.status).toBe(200);
-  
+
     // Verifique se o corpo da resposta contém as informações corretas
     expect(res.body.username).toBe("Usuário Teste");
     expect(res.body.ongname).toBe("ONG Existente");
+    console.log("RES BODY MESSAGE:")
+    console.log(res.body.message);
     expect(res.body.message).toBe("O usuário já está logado."); // [Parte mais importante do teste]
 
     // Verifique se um cookie de sessão foi setado
     expect(res.headers["set-cookie"]).toBeDefined();
-
   });
-  
+
+  test("Logout - Deve revogar a sessão e limpar o cookie do cliente.", async () => {
+    // Criação do usuário e sua sessão:
+    mock.onPost(process.env.API_LINK).reply(200, {
+      message: "Login bem-sucedido",
+      user: { name: "Usuário Teste" },
+      ngo: { id: "123", name: "ONG Teste" },
+    });
+
+    const registrarUsuario = await request(app)
+      .post("/usuarios/login")
+      .send({ email: "teste@email.com", password: "senha123" });
+
+    const user = await User.findOne({ email: "teste@email.com" });
+
+    const session = await Session.findOne({ id_usuario: user._id });
+    expect(session).not.toBeNull();
+
+    // Criação da sessão e token falso.
+    let req, res;
+    req = { token: session.token };
+    res = {
+      clearCookie: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await logout(req, res);
+    expect(res.clearCookie).toHaveBeenCalledWith("token");
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "A sessão foi revogada com sucesso.",
+    });
+
+    const acharSessao = await Session.findOne({
+      id_usuario: user._id,
+    });
+
+    expect(acharSessao).toBeNull();
+  });
+
+  test("Membros da ONG - Deve retornar todos os membros de uma ONG", async () => {
+    // Criando uma ONG no banco
+    const ong = await Ong.create({ id_gov: 123, nome: "ONG Existente" });
+
+    // Criando um usuário no banco
+    const user = await User.create({
+      email: "teste@email.com",
+      nome: "Usuário Teste",
+      id_ong: ong._id,
+    });
+
+    const user2 = await User.create({
+      email: "teste2@email.com",
+      nome: "Usuário Teste 2",
+      id_ong: ong._id,
+    });
+
+    req = { ongId: ong._id };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const listaMembros = await User.find({id_ong: ong._id});
+    await members(req, res);
+    
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message:'Os membros foram retornados com sucesso.',
+      membros: listaMembros
+    })
+  });
 });
